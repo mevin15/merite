52aa617621633f34fece435fb97c3737516c9461
herve grall <herve.grall@imt-atlantique.fr>
2017-06-27 17:57:07 +0200

    web sockets : francisation des noms

--- src/communication/tests/webSockets/testWebSocket.js
+++ src/communication/tests/webSockets/testWebSocket.js
@@ -1,32 +1,32 @@
-var http = require('http');
-var server = http.createServer(function(request, response) {});
+const http = require('http');
+const serveur = http.createServer(function(requete, reponse) {});
 
-server.listen(1234, function() {
-    console.log((new Date()) + ' Server is listening on port 1234');
+const port = 1234;
+
+serveur.listen(port, function() {
+    console.log((new Date()) + " Le serveur écoute le port " + port + ".");
 });
 
-var WebSocketServer = require('websocket').server;
-wsServer = new WebSocketServer({
-    httpServer: server
+const ServeurWebSocket = require('websocket').server;
+const serveurWS = new ServeurWebSocket ({
+    httpServer: serveur
 });
 
-var count = 0;
+var total = 0;
 var clients = {};
 
-
-wsServer.on('request', function(r){
-    var connection = r.accept('echo-protocol', r.origin);
-    // Specific id for this client & increment count
-    var id = count++;
+serveurWS.on('request', function(r){
+    var connexion = r.accept('echo-protocol', r.origin);
+    let id = total++;
     // Store the connection method so we can loop through & contact all clients
-    clients[id] = connection
-    console.log((new Date()) + ' Connection accepted [' + id + ']');
+    clients[id] = connexion;
+    console.log((new Date()) + " Connexion acceptée [" + id + "]");
 
     // Create event listener
-    connection.on('message', function(message) {
+    connexion.on('message', function(message) {
 
 	// The string message that was sent to us
-	var msgString = message.utf8Data;
+	var mot = message.utf8Data;
 
 	// Loop through all clients
 	for(var i in clients){
@@ -36,14 +36,14 @@ wsServer.on('request', function(r){
 		
 	    }else{
 		console.log("Diffusion de " + id + " vers " + i);
-		clients[i].sendUTF(msgString);
+		clients[i].sendUTF(mot);
 	    }
 	}
 	
     });
 
-    connection.on('close', function(reasonCode, description) {
+    connexion.on('close', function(reasonCode, description) {
 	delete clients[id];
-	console.log((new Date()) + ' Peer ' + id + " at " + connection.remoteAddress + ' disconnected.');
+	console.log((new Date()) + " Client " + id + " à l'adresse " + connexion.remoteAddress + " déconnecté.");
     });
 });
